0a9a5b606a4d7d7662877346b7b3ac40bc4a9364
Pedro Lucas <101678622+PedroLucasNeto@users.noreply.github.com>
2024-06-02 15:49:16 -0300

    feat: comment cart

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-const cors = require('cors');
+const cors = require("cors");
 
 app.use(cors());
 
@@ -10,95 +10,94 @@ app.use(express.json());
 
 let products = [];
 
-app.get('/products', (req, res) => {
+app.get("/products", (req, res) => {
   res.json(products);
 });
 
-app.get('/products/:id', (req, res) => {
+app.get("/products/:id", (req, res) => {
   const id = req.params.id;
   const product = products.find((p) => p.id === id);
   if (product) {
     res.json(product);
   } else {
-    res.status(404).json({ error: 'Product not found' });
+    res.status(404).json({ error: "Product not found" });
   }
 });
 
-app.post('/products', (req, res) => {
+app.post("/products", (req, res) => {
   const body = req.body;
-  const product = {...body, id: String(products.length + 1)}
+  const product = { ...body, id: String(products.length + 1) };
   products.push(product);
   res.status(201).json(product);
 });
 
-app.put('/products/:id', (req, res) => {
+app.put("/products/:id", (req, res) => {
   const id = req.params.id;
   const productIndex = products.findIndex((p) => p.id === id);
   if (productIndex !== -1) {
     products[productIndex] = { ...products[productIndex], ...req.body };
     res.json(products[productIndex]);
   } else {
-    res.status(404).json({ error: 'Product not found' });
+    res.status(404).json({ error: "Product not found" });
   }
 });
 
-app.delete('/products/:id', (req, res) => {
+app.delete("/products/:id", (req, res) => {
   const id = req.params.id;
   const productIndex = products.findIndex((p) => p.id === id);
   if (productIndex !== -1) {
     const deletedProduct = products.splice(productIndex, 1);
     res.json(deletedProduct[0]);
   } else {
-    res.status(404).json({ error: 'Product not found' });
+    res.status(404).json({ error: "Product not found" });
   }
 });
 
+// let cart = [];
+// app.get('/cart', (req, res) => {
+//   const cartItems = products.filter((product) => product.quantity > 0);
+//   const totalQuantity = cartItems.reduce((total, product) => total + product.quantity, 0);
+//   res.json({ items: cartItems, quantity: totalQuantity });
+// });
 
-let cart = [];
-app.get('/cart', (req, res) => {
-  const cartItems = products.filter((product) => product.quantity > 0);
-  const totalQuantity = cartItems.reduce((total, product) => total + product.quantity, 0);
-  res.json({ items: cartItems, quantity: totalQuantity });
-});
+// app.post('/cart', (req, res) => {
+//   const productId = req.body.productId;
+//   const product = products.find((p) => p.id === productId);
+//   if (product) {
+//     product.quantity = product.quantity || 0;
+//     product.quantity++;
+//     res.status(201).json(product);
+//   } else {
+//     res.status(404).json({ error: 'Product not found' });
+//   }
+// });
 
-app.post('/cart', (req, res) => {
-  const productId = req.body.productId;
-  const product = products.find((p) => p.id === productId);
-  if (product) {
-    product.quantity = product.quantity || 0;
-    product.quantity++;
-    res.status(201).json(product);
-  } else {
-    res.status(404).json({ error: 'Product not found' });
-  }
-});
+// app.put('/cart/:id', (req, res) => {
+//   const productId = req.params.id;
+//   const product = products.find((p) => p.id === productId);
+//   if (product) {
+//     const quantity = req.body.quantity;
+//     if (quantity < 1) {
+//       product.quantity = 0;
+//     } else {
+//       product.quantity = quantity;
+//     }
+//     res.json(product);
+//   } else {
+//     res.status(404).json({ error: 'Product not found in cart' });
+//   }
+// });
 
-app.put('/cart/:id', (req, res) => {
-  const productId = req.params.id;
-  const product = products.find((p) => p.id === productId);
-  if (product) {
-    const quantity = req.body.quantity;
-    if (quantity < 1) {
-      product.quantity = 0;
-    } else {
-      product.quantity = quantity;
-    }
-    res.json(product);
-  } else {
-    res.status(404).json({ error: 'Product not found in cart' });
-  }
-});
-
-app.delete('/cart/:id', (req, res) => {
-  const productId = req.params.id;
-  const product = products.find((p) => p.id === productId);
-  if (product) {
-    product.quantity = 0;
-    res.json(product);
-  } else {
-    res.status(404).json({ error: 'Product not found in cart' });
-  }
-});
+// app.delete('/cart/:id', (req, res) => {
+//   const productId = req.params.id;
+//   const product = products.find((p) => p.id === productId);
+//   if (product) {
+//     product.quantity = 0;
+//     res.json(product);
+//   } else {
+//     res.status(404).json({ error: 'Product not found in cart' });
+//   }
+// });
 
 // Start the server
 const port = 3000;
